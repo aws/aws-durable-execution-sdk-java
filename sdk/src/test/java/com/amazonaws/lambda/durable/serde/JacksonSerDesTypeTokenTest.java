19630d6e6dacb815fb575873429d0e4f62cf5295
Frank Chen <frankchn@amazon.com>
2026-01-26 10:25:44 -0800

    revert the accidental changes made by IntelliJ

--- sdk/src/test/java/com/amazonaws/lambda/durable/serde/JacksonSerDesTypeTokenTest.java
+++ sdk/src/test/java/com/amazonaws/lambda/durable/serde/JacksonSerDesTypeTokenTest.java
@@ -21,7 +21,7 @@ class JacksonSerDesTypeTokenTest {
     }
 
     @Test
-    void testDeserializeListGetStrings() {
+    void testDeserializeListOfStrings() {
         var json = "[\"apple\",\"banana\",\"cherry\"]";
         var token = new TypeToken<List<String>>() {};
 
@@ -35,7 +35,7 @@ class JacksonSerDesTypeTokenTest {
     }
 
     @Test
-    void testDeserializeMapGetStringToInteger() {
+    void testDeserializeMapOfStringToInteger() {
         var json = "{\"one\":1,\"two\":2,\"three\":3}";
         var token = new TypeToken<Map<String, Integer>>() {};
 
@@ -62,7 +62,7 @@ class JacksonSerDesTypeTokenTest {
     }
 
     @Test
-    void testDeserializeListGetMaps() {
+    void testDeserializeListOfMaps() {
         var json = "[{\"name\":\"Alice\",\"age\":30},{\"name\":\"Bob\",\"age\":25}]";
         var token = new TypeToken<List<Map<String, Object>>>() {};
 
@@ -175,7 +175,7 @@ class JacksonSerDesTypeTokenTest {
     }
 
     @Test
-    void testDeserializeListGetCustomObjects() {
+    void testDeserializeListOfCustomObjects() {
         var json = "[{\"name\":\"test1\",\"value\":10},{\"name\":\"test2\",\"value\":20}]";
         var token = new TypeToken<List<TestObject>>() {};
 
