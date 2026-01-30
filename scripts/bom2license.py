2b0011fa60e1aa0546bc68affcf4a8589306e400
Frank Chen <frankchn@amazon.com>
2026-01-29 17:11:29 -0800

    fix bom2license script

--- scripts/bom2license.py
+++ scripts/bom2license.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import xml.etree.ElementTree as ET
 
 # Define the CycloneDX namespace
-namespace = {'cyclonedx': ''}
+namespace = {'cyclonedx': 'http://cyclonedx.org/schema/bom/1.4'}
 
 # Parse the SBOM XML file
 tree = ET.parse('target/bom.xml')
@@ -12,14 +13,14 @@ with open('THIRD_PARTY', 'w') as f:
     f.write("Legal Notices Attribution File\n\n")
 
     # Iterate over each component in the SBOM
-    for component in root.findall(".cyclonedx:component", namespace):
-        name = component.find("cyclonedx:name", namespace).text
-        version = component.find("cyclonedx:version", namespace).text
-        license_element = component.find(".cyclonedx:id", namespace)
-        license_id = license_element.text if license_element is not None else "No License Found"
-        license_url_element = component.find(".cyclonedx:url", namespace)
-        license_url = license_url_element.text if license_url_element is not None else "No License URL"
-        copyright_element = component.find("cyclonedx:publisher", namespace)
+    for component in root.findall(".//cyclonedx:component", namespace):
+        name = component.find("./cyclonedx:name", namespace).text
+        version = component.find("./cyclonedx:version", namespace).text
+        license_elements = component.findall("./cyclonedx:licenses//cyclonedx:id", namespace)
+        license_id = ','.join(l.text for l in license_elements) if license_elements else "No License Found"
+        license_url_elements = component.findall("./cyclonedx:licenses//cyclonedx:url", namespace)
+        license_url = ','.join(l.text for l in license_url_elements) if license_url_elements else "No License URL"
+        copyright_element = component.find("./cyclonedx:publisher", namespace)
         copyright = copyright_element.text if copyright_element is not None else "No Copyright"
 
         # Write the component information to the attribution file
