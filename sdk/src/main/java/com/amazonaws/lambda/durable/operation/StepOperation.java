5b62bba53c0adc7eb74eb87a2152a69b58d0bf9b
Frank Chen <frankchn@amazon.com>
2026-02-06 16:46:06 -0800

    remove the non-relevant comment

--- sdk/src/main/java/com/amazonaws/lambda/durable/operation/StepOperation.java
+++ sdk/src/main/java/com/amazonaws/lambda/durable/operation/StepOperation.java
@@ -47,7 +47,6 @@ public class StepOperation<T> extends BaseDurableOperation<T> {
             ExecutionManager executionManager,
             DurableLogger durableLogger,
             DurableConfig durableConfig) {
-        // Use custom SerDes from config if provided, otherwise use default
         super(operationId, name, OperationType.STEP, resultTypeToken, config.serDes(), executionManager);
 
         this.function = function;
