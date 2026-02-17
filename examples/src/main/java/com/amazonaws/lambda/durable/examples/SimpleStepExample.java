e752e6debdc48cb57080f79462a5e24e541c124a
hsilan <hsilan@amazon.com>
2026-02-17 10:30:11 -0800

    chore: remove unused import

--- examples/src/main/java/com/amazonaws/lambda/durable/examples/SimpleStepExample.java
+++ examples/src/main/java/com/amazonaws/lambda/durable/examples/SimpleStepExample.java
@@ -5,8 +5,6 @@ package com.amazonaws.lambda.durable.examples;
 import com.amazonaws.lambda.durable.DurableContext;
 import com.amazonaws.lambda.durable.DurableHandler;
 
-import java.time.Duration;
-
 /**
  * Simple example demonstrating basic step execution with the Durable Execution SDK.
  *
